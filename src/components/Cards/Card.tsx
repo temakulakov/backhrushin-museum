6568fc52dc6b8906e609ec8c965e4a538b8d252e
Artem Kulakov <109291017+temakulakov@users.noreply.github.com>
2024-04-04 15:31:46 +0300

    init

--- src/components/Cards/Card.tsx
+++ src/components/Cards/Card.tsx
@@ -22,7 +22,8 @@ const Card = ({duration, imageUrl, title, position, type, link}: ICardProps) => 
         <motion.div className={styles.wrapper}
                     onMouseEnter={() => setHover(true)}
                     onMouseLeave={() => setHover(false)}
-                    animate={{backgroundColor: hover ? "#52565A" : 'white', padding: 0, marginRight: '30px'}}
+                    // animate={{backgroundColor: hover ? "#52565A" : 'white', padding: 0, marginRight: '30px'}}
+                    animate={{ padding: 0, marginRight: '30px'}}
         >
             <motion.div
                 className={styles.imageWrapper}
@@ -35,18 +36,24 @@ const Card = ({duration, imageUrl, title, position, type, link}: ICardProps) => 
                 />
                 {type && <div className={styles.type}/>}
             </motion.div>
-            <motion.p animate={{color: hover ? 'white' : '#52565A'}}
-                      className={styles.date}>{`${duration.from.format('D MMMM')} - ${duration.to.format('D MMMM')}`}</motion.p>
-            <motion.p animate={{color: hover ? 'white' : '#52565A'}} className={styles.title}>{title}</motion.p>
+            {/*<motion.p animate={{color: hover ? 'white' : '#52565A'}}*/}
+            <p
+                      className={styles.date}>{`${duration.from.format('D MMMM')} - ${duration.to.format('D MMMM')}`}</p>
+            {/*<motion.p animate={{color: hover ? 'white' : '#52565A'}} className={styles.title}>{title}</motion.p>*/}
+            <p className={styles.title}>{title}</p>
 
-            <motion.p animate={{color: hover ? 'white' : '#52565A'}} className={styles.position}>
+            {/*<motion.p animate={{color: hover ? 'white' : '#52565A'}} className={styles.position}>*/}
+            <p className={styles.position}>
                 <motion.svg width="10" height="12" viewBox="0 0 10 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <motion.circle cx="5" cy="5" r="5" animate={{fill: !hover ? "#53565A" : 'white'}}/>
-                    <motion.path d="M5 12L1 8H9L5 12Z" animate={{fill: !hover ? "#53565A" : 'white'}}/>
-                    <motion.circle cx="5" cy="5" r="2" animate={{fill: !hover ? "white" : '#53565A'}}/>
+                    {/*<motion.circle cx="5" cy="5" r="5" animate={{fill: !hover ? "#53565A" : 'white'}}/>*/}
+                    <motion.circle cx="5" cy="5" r="5" animate={{fill: !hover ? "#53565A" : '#53565A'}}/>
+                    {/*<motion.path d="M5 12L1 8H9L5 12Z" animate={{fill: !hover ? "#53565A" : 'white'}}/>*/}
+                    <motion.path d="M5 12L1 8H9L5 12Z" animate={{fill: !hover ? "#53565A" : '#53565A'}}/>
+                    {/*<motion.circle cx="5" cy="5" r="2" animate={{fill: !hover ? "white" : '#53565A'}}/>*/}
+                    <motion.circle cx="5" cy="5" r="2" animate={{fill: !hover ? "white" : 'white'}}/>
                 </motion.svg>
 
-                {position}</motion.p>
+                {position}</p>
         </motion.div>
     </Link>
 };
